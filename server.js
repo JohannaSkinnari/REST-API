a7c6313ded4d318a54d4c80402937c2fc7a9bf0f
Johanna Skinnari <jskinnari@gmail.com>
2021-08-18 12:58:03 +0200

    add basic logic plus endpoints to server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const app = express();
+
 const books = [{
     id: 0,
     title: 'tintin',
@@ -10,3 +13,26 @@ const books = [{
     author: 'Erik',
     year: 1990
 }];
+
+app.use(express.json());
+
+// Resource Endpoints
+app.get('/api/books', (req, res, next) => {
+    res.json(books);
+});
+app.get('/api/books/:id', (req, res, next) => {
+
+});
+app.post('/api/books', (req, res, next) => {
+    
+});
+app.put('/api/books/:id', (req, res, next) => {
+
+});
+app.delete('/api/books/:id', (req, res, next) => {
+
+});
+
+app.listen(3000, () => {
+    console.log('Server is running!');
+});
