40334a4f225d8fc4a65593ff709c9657f2f0a43e
Johanna Skinnari <jskinnari@gmail.com>
2021-08-20 16:37:35 +0200

    make renderBooks function in script.js render objects

--- public/script.js
+++ public/script.js
@@ -2,12 +2,12 @@ window.addEventListener('load', main);
 
 const books = [];
 
-function main() {
-    laodBooks();
-    // renderBooks();
+async function main() {
+    await loadBooks();
+    renderBooks();
 }
 
-async function laodBooks() {
+async function loadBooks() {
     const response = await fetch('http://localhost:3000/api/books');
     const data = await response.json();
     for (const book of data){
@@ -16,14 +16,23 @@ async function laodBooks() {
     return books;
 }
 
-// function renderBooks() {
-//     const listContainder = document.querySelector('listContainer');
-//     listContainder.innerHTML = "";
-//     books.forEach(function(book) {
-//         const bookItem = document.createElement('li');
-//         bookItem.className = 'bookItem';
-//         const bookContent = createElement(book);
-//         bookItem.innerHTML = bookContent;
-//         listContainder.append(bookItem);
-//     })
-// }
+function renderBooks() {
+    const listContainer = document.querySelector('.listContainer');
+    console.log({listContainer})
+    listContainer.innerHTML = "";
+    console.log({books});
+    console.log('books.length: ', books.length)
+    books.forEach((book) =>  {
+        console.log(book)
+        const bookItem = document.createElement('div');
+        bookItem.className = 'bookItem';
+        const bookContent = 
+        `<h5>${book.title}</h5>
+        <div>Author: ${book.author}</div>
+        <div>Year: ${book.year}</div>
+        <button>Delete</button>
+        <button>Edit</button>`
+        bookItem.innerHTML = bookContent;
+        listContainer.append(bookItem);
+    });
+}
