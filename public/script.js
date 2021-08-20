5e02f8f80be25bd24403478e484d2299a8c463c1
Johanna Skinnari <jskinnari@gmail.com>
2021-08-20 17:01:01 +0200

    make loadBook and renderBook in script.js, make them work

--- public/script.js
+++ public/script.js
@@ -1,10 +1,13 @@
 window.addEventListener('load', main);
 
-const books = [];
+let books = [];
+let book = {};
 
 async function main() {
-    await loadBooks();
-    renderBooks();
+    // await loadBooks();
+    // renderBooks();
+    await loadBook("34b26b00-018c-11ec-9d0f-2b955942d1c4");
+    renderBook();
 }
 
 async function loadBooks() {
@@ -18,21 +21,42 @@ async function loadBooks() {
 
 function renderBooks() {
     const listContainer = document.querySelector('.listContainer');
-    console.log({listContainer})
     listContainer.innerHTML = "";
-    console.log({books});
-    console.log('books.length: ', books.length)
     books.forEach((book) =>  {
-        console.log(book)
         const bookItem = document.createElement('div');
         bookItem.className = 'bookItem';
         const bookContent = 
         `<h5>${book.title}</h5>
         <div>Author: ${book.author}</div>
         <div>Year: ${book.year}</div>
+        <button>Details</button>
         <button>Delete</button>
         <button>Edit</button>`
         bookItem.innerHTML = bookContent;
         listContainer.append(bookItem);
     });
+}
+
+
+async function loadBook(id) {
+    const response = await fetch('http://localhost:3000/api/books/' + id);
+    book = await response.json();
+
+    return book;
+}
+
+function renderBook() {
+    const listContainer = document.querySelector('.listContainer');
+    listContainer.innerHTML = "";
+    const bookItem = document.createElement('div');
+    bookItem.className = 'bookItem';
+    const bookContent = 
+    `<h5>${book.title}</h5>
+    <div>Author: ${book.author}</div>
+    <div>Year: ${book.year}</div>
+    <button>Back</button>
+    <button>Delete</button>
+    <button>Edit</button>`
+    bookItem.innerHTML = bookContent;
+    listContainer.append(bookItem);
 }
